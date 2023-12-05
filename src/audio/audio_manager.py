f4e8f7379d858f2aec29aad9be95837e64f675f5
Jorge Aguirregomezcorta <jaguirregomezcorta@gmail.com>
2023-12-05 12:08:10 +0100

    Add Speech Recognition to Pipeline
    Transform the shitfuckery Jesper did and add it to the main program pipeline. Modify audio cache folders and update requirements

--- src/audio/audio_manager.py
+++ src/audio/audio_manager.py
@@ -10,11 +10,11 @@ AUDIO_FORMAT = pyaudio.paInt16
 AUDIO_CHANNELS = 1
 AUDIO_RATE = 16000
 AUDIO_CHUNK = 1024
-TIMEOUT_LENGTH = 5
+TIMEOUT_LENGTH = 3
 THRESHOLD = 10
 SWIDTH = 2
 SHORT_NORMALIZATION = (1.0/32768.0)
-AUDIO_DIRECTORY = "./audio_dir"
+AUDIO_DIRECTORY = "./audio_cache"
 
 class Audio_Manager:
     '''
@@ -42,29 +42,33 @@ class Audio_Manager:
         rms = math.pow(sum_squares / count, 0.5)
         return rms * 1000
     
-    def write(self, recording):
+    def write(self, recording) -> str:
         '''
         Save audio recordings into .wav format files
         '''
+        number_frames_to_remove = TIMEOUT_LENGTH * AUDIO_RATE
+        recording = recording[: len(recording) - number_frames_to_remove]
         number_files = len(os.listdir(AUDIO_DIRECTORY))
-        filename = os.path.join(AUDIO_DIRECTORY, '{}.wav'.format(number_files))
+        file_name = '{}.wav'.format(number_files)
+        file_path = os.path.join(AUDIO_DIRECTORY, file_name)
 
-        with wave.open(filename, 'wb') as wf:
+        with wave.open(file_path, 'wb') as wf:
             wf.setnchannels(AUDIO_CHANNELS)
             wf.setsampwidth(self.audio.get_sample_size(AUDIO_FORMAT))
             wf.setframerate(AUDIO_RATE)
             wf.writeframes(recording)
             wf.close()
         
-        print('Written to file: {}'.format(filename))
+        print('Written to file: {}'.format(file_path))
         print('Returning to listening')
+        return file_name
     
-    def record(self):
+    def record(self, initial_chunk) -> str:
         '''
         Record audio until the timeout is completed
         '''
         print('Beginning recording...')
-        recording = []
+        recording = [initial_chunk]
         current_time = time.time()
         end_time = current_time + TIMEOUT_LENGTH
 
@@ -74,17 +78,15 @@ class Audio_Manager:
                 end_time = time.time() + TIMEOUT_LENGTH
             current_time = time.time()
             recording.append(data)
-        self.write(b''.join(recording))
+        return self.write(b''.join(recording))
 
     def listen(self):
         '''
         Listen and record user input until waiting threshold has been surpassed
         '''
         print('Listening...')
-        listen = True
-        while listen:
+        while True:
             input = self.stream.read(AUDIO_CHUNK)
             rms_value = self.rms(input)
             if rms_value > THRESHOLD:
-                self.record()
-                listen = False
+                return self.record(input)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,36 @@
 # Import libraries
+import cv2 as cv
 from audio.audio_manager import Audio_Manager
+from speech_recognition.speech_recognition import Speech_Recognizer
 
-# Main function
+
 def main():
-    print("Main function")
+    '''
+    Main thread of the application
+    '''
     audio_manager = Audio_Manager()
-    audio_manager.listen()
+    speech_recognizer = Speech_Recognizer()
+
+    audio_file = audio_manager.listen()
+    speech_language = speech_recognizer.classify_audio(audio_file)
+    '''
+    For Oskar:
+        Speech_Language can be { "es: Spanish","en: English", "sv: Swedish" }
+    '''
+
+def translation(audio_manager: Audio_Manager, speech_recognizer: Speech_Recognizer):
+    '''
+    Execute translation program pipeline
+    '''
+    canListen = True
+
+    while True:
+        if canListen:
+            audio_manager.listen()
+        canListen = False
+        # Stop application if user presses 'Q'
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
 
 # Main program of the application
 if __name__ == '__main__':
